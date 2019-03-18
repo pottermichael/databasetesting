adc9c478d3f13df162cc2a989138c72549682848
pottermichael <potter.michael@gmail.com>
2019-03-18 18:58:27 -0500

    after rebooting server

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get 'history', to: 'home#history'
+
+  get 'amenity', to: 'home#amenity'
+
+  get 'migration', to: 'home#migration'
+
+  get 'comps', to: 'home#comps'
+
+  get 'apartment', to: 'home#apartment'
+
+  get 'inventory', to: 'home#inventory'
+
+  get 'infrastructure', to: 'home#infrastructure'
+
+  get 'timeseries', to: 'home#timeseries'
+
+end
