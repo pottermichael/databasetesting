49cddf5d7fd00761d21c4e1a3e735cee49d986f2
pottermichael <potter.michael@gmail.com>
2019-04-14 14:48:05 -0500

    finalized layer toggle in layer view for mapbox incl CDB and NorthBranch

--- app/assets/javascripts/beacon.js
+++ app/assets/javascripts/beacon.js
@@ -5,20 +5,58 @@ var initialRadius = 10;
 var radius = initialRadius;
 var maxRadius = 50;
 
-function animateMarker(timestamp) {
-  setTimeout(function(){
-      requestAnimationFrame(animateMarker);
+map.on('load', function () {
 
-      radius += (maxRadius - radius) / framesPerSecond;
-      opacity -= ( .9 / framesPerSecond );
+    // Add a source and layer displaying a point which will be animated in a circle.
+    map.addSource('point', {
+        "type": "geojson",
+        "data": {
+            "type": "Point",
+            "coordinates": [
+                -87.646164, 41.894611
+            ]
+        }
+    });
 
-      map.setPaintProperty('point', 'circle-radius', radius);
-      map.setPaintProperty('point', 'circle-opacity', opacity);
+    map.addLayer({
+        "id": "point",
+        "source": "point",
+        "type": "circle",
+        "paint": {
+            "circle-radius": initialRadius,
+            "circle-radius-transition": {duration: 0},
+            "circle-opacity-transition": {duration: 0},
+            "circle-color": "#ffffff"
+        }
+    });
 
-      if (opacity <= 0) {
-          radius = initialRadius;
-          opacity = initialOpacity;
-      }
+    map.addLayer({
+        "id": "point1",
+        "source": "point",
+        "type": "circle",
+        "paint": {
+            "circle-radius": initialRadius,
+            "circle-color": "#007cbf"
+        }
+    });
 
-  }, 1000 / framesPerSecond);
-}
+
+    function animateMarker(timestamp) {
+        setTimeout(function(){
+            requestAnimationFrame(animateMarker);
+
+            radius += (maxRadius - radius) / framesPerSecond;
+            opacity -= ( .9 / framesPerSecond );
+
+            map.setPaintProperty('point', 'circle-radius', radius);
+            map.setPaintProperty('point', 'circle-opacity', opacity);
+
+            if (opacity <= 0) {
+                radius = initialRadius;
+                opacity = initialOpacity;
+            }
+
+        }, 1000 / framesPerSecond);
+
+    }
+});
