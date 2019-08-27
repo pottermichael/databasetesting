d37654adf7365e643e4084f520e7e2e077bee8b8
pottermichael <potter.michael@gmail.com>
2019-08-27 10:07:59 -0500

    added "layer" view and 777 landscape plan along coordinates over the site

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'banksites', to: 'home#banksites'
 
+  get 'layer', to: 'home#layer'
+
   get 'resiall', to: 'home#resiall'
 
   get 'satellite', to: 'home#satellite'
