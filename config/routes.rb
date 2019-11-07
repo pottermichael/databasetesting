c2162eddff309bd8a3bca5da4be8be1bbbafe9d6
pottermichael <potter.michael@gmail.com>
2019-11-07 15:13:42 -0600

    delete obsolete views, added rivertour view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'playbutton', to: 'home#playbutton'
 
+  get 'rivertour', to: 'home#rivertour'
+
   get 'planned', to: 'home#planned'
 
   get 'finished', to: 'home#finished'
