486b52008cace09187a5dd8ea1593b6e1c751220
pottermichael <potter.michael@gmail.com>
2020-01-10 15:09:43 -0600

    walk

--- app/assets/javascripts/configbank.js
+++ app/assets/javascripts/configbank.js
@@ -236,7 +236,7 @@ var config = {
     },
     {
       id: 'cta_walksheds',
-      title: 'CTA Transit Access',
+      title: 'Walk to Transit',
       description: 'Chicago CTA Blue and Brown lines currently service the most desirable neighborhoods for corporate talent recruitment. An evaluation of .5 mile walksheds from CTA stations illustrates that transit coverage for the River District is unmatched.',
       location: {
         center: [-87.65980, 41.88548],
